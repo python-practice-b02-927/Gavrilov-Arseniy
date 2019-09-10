58043bb6ed8f6d0ea45b60a0827ea47cb9c94567
gavrilovarsenii <55045365+gavrilovarsenii@users.noreply.github.com>
2019-09-10 23:41:11 +0300

    Update task_11.py

--- robotgaa/task_11.py
+++ robotgaa/task_11.py
@@ -5,7 +5,7 @@ from pyrob.api import *
 
 @task
 def task_8_4():
-    while not(wall_is_on_the_right()):
+    while not (wall_is_on_the_right()):
         if (wall_is_above()) and (wall_is_beneath()):
             fill_cell()
         move_right()
