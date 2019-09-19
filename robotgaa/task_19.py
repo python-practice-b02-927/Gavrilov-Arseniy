9f90e1cc33fdc19852844ce4231eab0536d0876a
gavrilovarsenii <55045365+gavrilovarsenii@users.noreply.github.com>
2019-09-19 19:25:13 +0300

    Update task_19.py

--- robotgaa/task_19.py
+++ robotgaa/task_19.py
@@ -5,26 +5,20 @@ from pyrob.api import *
 
 @task
 def task_8_29():
-    while not (wall_is_on_the_left()):
+    while not wall_is_on_the_left():
         move_left()
-    if not (wall_is_above()):
-        while not (wall_is_above()):
-            move_up()
-            
-    
-    while not (wall_is_on_the_right()):
+    while not wall_is_above():
+        move_up()
+    while wall_is_beneath():
         move_right()
-    if not (wall_is_above()):
-        while not (wall_is_above()):
-            move_up()
-            while not (wall_is_on_the_left()):
-                move_left
-                
-
-    
-    
-    
-
-
+        if wall_is_on_the_right():
+            break
+    while not wall_is_above():
+        move_up()
+    while not wall_is_on_the_left() and not wall_is_beneath():
+        move_left()
+        
+        
+      
 if __name__ == '__main__':
-    run_tasks()
+    run_tasks()  
